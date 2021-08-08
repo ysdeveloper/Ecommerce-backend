bd381dc754e1164d69584e95b403963c59bc8f8c
darkwarrior303 <ysdeveloper1998@gmail.com>
2021-08-08 21:39:59 +0530

    Project Setup.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a019c247a7d7868e70415b7293fbb699'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9f4f1627-9e6f-4f40-846a-0a1f58db777c'
+};
